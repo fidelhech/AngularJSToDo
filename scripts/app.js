872df8079a84570585d8a5d4d835c51cbdd0b639
Fidel Hechavarria <fidelh@IVGHQ.COM>
2017-01-25 13:55:42 -0500

    Showing json and added more directives.

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,15 @@
 angular.module('todoListApp', [])
 .controller('mainCtrl', function($scope) {
+
 	$scope.helloWorld = function() {
 		console.log('Hello There! HelloWorld Controller in the MainCtrl.');
 	};
+
+	$scope.todos = [
+		{'name': 'First Todo'},
+		{'name': 'Second Todo'},
+		{'name': 'Third Todo'},
+		{'name': 'Fourth Todo'},
+		{'name': 'Fifth Todo'},
+	]
 });
